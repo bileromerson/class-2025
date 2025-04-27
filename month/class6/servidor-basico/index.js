7b8b22489c5738d269ec6cfe5bf9f03112bf7620
bileromerson <158579506+bileromerson@users.noreply.github.com>
2025-04-27 18:31:00 -0300

    main

---
+++ month/class6/servidor-basico/index.js
@@ -0,0 +1,36 @@
+
+console.log('ola, alunos do curso Node.js!');
+const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('resposta enviada com sucesso!');
+// });
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Bem Vindo');
+    } else if (req.url === '/amigos'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('aqui esta a lista de amigos');
+    }
+    else if (req.url === '/perfil'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('aqui esta o seu perfil');
+
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Página não encontrada');
+    }
+});
+
+server.listen(3000,() => {
+    console.log('servidor rodando na porta 3000');
+});
+
